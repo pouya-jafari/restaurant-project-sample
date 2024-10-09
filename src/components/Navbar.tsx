af2df9bc727ce26ad053c453b87dac55f2225cf2
Pouya Jafari <pouyajefri1080@outlook.com>
2024-10-08 20:40:07 -0700

    initialization of the project

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  <div className="navbar">
+    <img src="logo.png" alt="" className="logo" />
+
+    <ul>
+      <li>
+        <a href=""></a>
+      </li>
+    </ul>
+  </div>;
+}
