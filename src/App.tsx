cd1c24d3909f1b501ecc6eb39c12dbff1d2d7668
Pouya Jafari <pouyajefri1080@outlook.com>
2024-10-09 14:31:29 -0700

    styled the navbar and added some content to content div

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,18 @@ function App() {
     <>
       <div className="banner">
         <Navbar />
+        <div className="content">
+          <h1>طنجه</h1>
+          <p>طعم اصیل ایرانی</p>
+          <div>
+            <button type="button">
+              <span></span>منو
+            </button>
+            <button type="button">
+              <span></span>تماس بگیرید
+            </button>
+          </div>
+        </div>
       </div>
     </>
   );
