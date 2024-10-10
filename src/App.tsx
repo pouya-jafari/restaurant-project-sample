c77d913e61e8c7cbd42179369464bc8620e27d49
Pouya Jafari <pouyajefri1080@outlook.com>
2024-10-09 21:11:52 -0700

    created router and added the page folder to organize the app directory

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import Head from "./components/Head";
-import Detail from "./components/Detail";
+import RootLayout from "./pages/Root";
+import ErrorPage from "./pages/Error";
+import HomePage from "./pages/Home";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [{ index: true, path: "/", element: <HomePage /> }],
+  },
+]);
+
 function App() {
-  return (
-    <>
-      <Head />
-      <Detail />
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+// import { useNavigate } from "react-router-dom";
+import Head from "../components/Head";
+import Detail from "../components/Detail";
+function HomePage() {
+  //   const navigate = useNavigate();
+  //   //   function navigateHandler() {
+  //   //     navigate("/products");
+  //   //   }
+  return (
+    <>
+      <Head />
+      <Detail />
+    </>
+  );
+}
+
+export default HomePage;
